96896dd2da7d7627f1690178e25ee1395ded77cd
vitaminus <partadw@gmail.com>
2018-04-25 00:18:37 +0300

    Add transition

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -18,5 +20,35 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.fade-enter-active {
+  animation: slidePageIn 0.4s forwards;
+  animation-timing-function: cubic-bezier(.8, -0.49, .36, 1);
+}
+.fade-leave-active {
+  animation: slidePageDown 0.6s forwards;
+  animation-timing-function: cubic-bezier(.8, -0.49, .36, 1);
+}
+
+@keyframes slidePageIn {
+  from {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0%);
+  }
+}
+
+@keyframes slidePageDown {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
 }
 </style>
